43b465008aa2eb5eb7099c8e50cf308d58ed90dd
SoumyaZolute <spnayak@zolute.consulting>
2024-05-15 20:57:41 +0530

    fixed filenot found issue

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 export const getProjectDetails = async (fileName) => {
     try {
-        const filePath = path.resolve(`src/data/projects/${fileName}.json`);
+        const filePath = `src/data/projects/${fileName}.json`;
         const projectDetails = fs.readFileSync(filePath, "utf8");
 
         if (projectDetails) return { status: true, data: JSON.parse(projectDetails) };
